695f04f5665cf8b312883ded95538ea0bcdb181d
Tina Müller <tina.mueller@suse.com>
2020-06-19 17:25:04 +0200

    WIP

--- tools/cache-deps.sh
+++ tools/cache-deps.sh
@@ -15,12 +15,19 @@ mkdir -p $CACHEDIR
 resultfile="$CACHEDIR/$MD5.result"
 if [[ -e "$resultfile" ]]; then
     result="$(cat $resultfile)"
-    if [[ $result == 1 ]]; then
+    if [[ $result == 0 ]]; then
         echo "$resultfile exists and was successful"
         exit
     fi
 fi
 
 cp $DIFFDEPS "$CACHEDIR/$MD5.diff"
+result=1
+echo $result > "$resultfile"
+
 result=0
+./autogen.sh && make || result=1 && exit 1
+make check || result=1
+make test || result=1
+
 echo $result > "$resultfile"
